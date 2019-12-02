c925a8a303a43f55fd8176d53a8d89afbe2f9475
Fredd <fredd@neosdev.com.br>
2019-12-02 08:54:27 -0300

    main-endpoints

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const server = express();
 
+const projects = [{id: "1" , title:"novo projeto" , tasks:[ "Docs" , "teams" ] }]
+const prefix = "/projects";
+			
+
 server.use(express.json());
 
 
@@ -9,4 +13,50 @@ server.get('/hello' , (req,res) => {
 	console.log('hello world');
 	res.json({message:'hello world'});
 })
+
+
+server.get(prefix , (req,res) => {
+
+	return res.json(projects);
+
+});
+
+
+server.get(`${prefix}/:project_id` , (req,res) => {
+
+	const {project_id} = req.params
+	return res.json(projects[id]);
+
+});
+
+server.post(prefix , (req,res) => {
+
+	const {title , id} = req.body
+	const newProject = {title , id , tasks:[]}
+
+	projects.push(newProject);
+	return res.json(projects);
+
+})
+
+//TODO: ost task on projects
+server.post(`${prefix}/:project_id` , (req,res) => {
+
+	const {title , id} = req.body
+	const newProject = {title , id , tasks:[]}
+
+	projects.push(newProject);
+	return res.json(projects);
+
+})
+
+
+
+
+
+
+
+
+
+
 server.listen(3000);
