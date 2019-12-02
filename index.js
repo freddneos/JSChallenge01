1c6670f0459235bdd71618780d5f9e307f413d1d
Fredd <fredd@neosdev.com.br>
2019-12-02 13:42:54 -0300

    refactoring-some

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ server.use(express.json());
 
 
 
+function verifyIfProjectExist(req,res,next){
+
+	let {project_id } = req.params;
+	project_id = project_id - 1;
+	project = projects[project_id];
+	if(!project){
+		return res.status(400).json({error:"Project not found."})
+	}
+	//creating variable!
+	req.project = project;
+	return next();
+
+}
+
+function verifyIfTaskExist(req,res,next){
+	const {name }  = req.body;
+	const project = req.project;
+	console.log(project , project.tasks.indexOf(name));
+
+	if(project.tasks.indexOf(name) === -1){
+		return res.status(400).json({error:"Task not found."})
+	}
+	return next();
+}
+
 server.get('/hello' , (req,res) => {
 	console.log('hello world');
 	res.json({message:'hello world'});
@@ -22,10 +47,9 @@ server.get(prefix , (req,res) => {
 });
 
 
-server.get(`${prefix}/:project_id` , (req,res) => {
+server.get(`${prefix}/:project_id` ,verifyIfProjectExist, (req,res) => {
 
-	const {project_id} = req.params
-	return res.json(projects[id]);
+	return res.json(req.project);
 
 });
 
@@ -40,7 +64,7 @@ server.post(prefix , (req,res) => {
 })
 
 //TODO: ost task on projects
-server.post(`${prefix}/:project_id` , (req,res) => {
+server.post(`${prefix}/:project_id` ,verifyIfProjectExist, (req,res) => {
 
 	const {title , id} = req.body
 	const newProject = {title , id , tasks:[]}
@@ -50,8 +74,42 @@ server.post(`${prefix}/:project_id` , (req,res) => {
 
 })
 
+server.post(`${prefix}/:project_id/tasks` ,verifyIfProjectExist, (req,res) => {
+
+	const {project_id} = req.params;
+	const {name} = req.body;
+	const project = req.project;
+
+	project.tasks.push(name);
+
+	return res.json(project);
+
+})
 
 
+
+server.delete(`${prefix}/:project_id` ,verifyIfProjectExist, (req,res) => {
+
+	const project = req.project
+	console.log(project)
+	projects.splice( project.id.parseInt, 1);
+	return res.json();
+
+})
+
+server.delete(`${prefix}/:project_id/tasks` ,verifyIfProjectExist,verifyIfTaskExist, (req,res) => {
+
+	const {name} = req.body;
+	const project = req.project;
+
+	const taskToDelete = project.tasks.indexOf(name)
+
+	project.tasks.splice(taskToDelete , 1);
+	
+	
+	return res.json(project);
+
+})
 
 
 
